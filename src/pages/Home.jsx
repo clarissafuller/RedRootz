e8563c72fa01da47f8ec8df674f03c45cb869690
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2025-04-30 09:41:17 -0400

    adds base sections to home page layout and imports assets needed for porject

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,30 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="home-wrapper">
+      {/* Hero */}
+      <section className="home-hero">
+        <div className="nav-bar"></div>
+        <img src={""} alt="red rootz logo" />
+      </section>
+
+      {/* Contact Section */}
+      <section className="contact"></section>
+
+      {/* Book Now */}
+      <section className="book-now"></section>
+
+      {/* Services */}
+      <section className="services"></section>
+
+      {/* The Team */}
+      <section className="team"></section>
+
+      {/* Team Breakdown */}
+      <section className="team-breakdown"></section>
+      {/* Footer */}
+      <footer className="footer-text">
+        <p>Built by Clarissa Fuller &copy; {new Date().getFullYear()}</p>
+      </footer>
+    </div>
+  );
 }
