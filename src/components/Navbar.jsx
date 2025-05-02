647ec70f47094d6f783e8cd8d302f12f21619668
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2025-05-02 14:04:35 -0400

    adds finished styling for home page on browswe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,37 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="nav-bar">
-      <a href="about">About</a>
-      <a href="#contact">Contact</a>
-      <a
-        href="https://hairbyredrootz.glossgenius.com/services"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Services
-      </a>
-      <a
-        href="https://hairbyredrootz.glossgenius.com/booking-flow"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Appointments
-      </a>
+      <div className="nav-left">
+        <a href="/about">About</a>
+        <a href="#contact">Contact</a>
+      </div>
+
+      <Link to="/" className="logo-link">
+        <img
+          src="src/assets/tigers.png"
+          alt="tiger logo"
+          className="tiger-logo"
+        />
+      </Link>
+
+      <div className="nav-right">
+        <a
+          href="https://hairbyredrootz.glossgenius.com/services"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Services
+        </a>
+        <a
+          href="https://hairbyredrootz.glossgenius.com/booking-flow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Appointments
+        </a>
+      </div>
     </nav>
   );
 }
