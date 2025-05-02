13f4ada61f477f1450c6877d45cc9c11d5021d77
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2025-05-02 13:02:55 -0400

    adds basic section layout for about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,15 @@
 export default function About() {
-  return <div>Hello World</div>;
+  return (
+    <div className="about-wrapper">
+      <section className="header">
+        <p>EVERYTHING YOU NEED TO KNOW</p>
+      </section>
+      <section className="cancellation-policy">
+        <p>dont cancel</p>
+      </section>
+      <section className="our-story">
+        <p>the story</p>
+      </section>
+    </div>
+  );
 }
